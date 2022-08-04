cf0b456d97ed1f25ed5dcbad15aaa98ab61d3cef
Nikolaj Kappler <nikolaj-kappler@web.de>
2022-08-04 21:30:40 +0200

    write tests, coverage 100%, some fixes
    fix #2

---
+++ src/index.test.ts
@@ -0,0 +1,359 @@
+import getDataProvider, { formatFilter, TreeQLDataProvider } from ".";
+
+
+class TestDataProvider extends TreeQLDataProvider {
+    public getURL(resource: string, params?: Record<string, string>): string {
+        return super.getURL(resource, params);
+    }
+}
+
+const mockHTTP = {
+    fetch: (_url: string, _options?) => Promise.resolve({ status: 200, headers: null as any, body: null, json: null })
+};
+
+const dataProvider = new TestDataProvider("http://myApi.com/", (url, options) => mockHTTP.fetch(url, options));
+
+const spyOn = jest.spyOn;
+
+describe("getDataProvider", () => {
+    it("default export should work", () => {
+        expect(getDataProvider("http://myApi.com") instanceof TreeQLDataProvider).toBeTruthy();
+    });
+});
+
+describe("formatFilter", () => {
+    it("default", () => expect(formatFilter({ comment: 4 })).toEqual("comment,eq,4"));
+    it("contains", () => expect(formatFilter({ comment_cs: 4 })).toEqual("comment,cs,4"));
+    it("start with", () => expect(formatFilter({ comment_sw: 4 })).toEqual("comment,sw,4"));
+    it("end with", () => expect(formatFilter({ comment_ew: 4 })).toEqual("comment,ew,4"));
+    it("lower than", () => expect(formatFilter({ comment_lt: 4 })).toEqual("comment,lt,4"));
+    it("lower or equal", () => expect(formatFilter({ comment_le: 4 })).toEqual("comment,le,4"));
+    it("greater or equal", () => expect(formatFilter({ comment_ge: 4 })).toEqual("comment,ge,4"));
+    it("greater than", () => expect(formatFilter({ comment_gt: 4 })).toEqual("comment,gt,4"));
+    it("between", () => expect(formatFilter({ comment_bt: [4, 8] })).toEqual("comment,bt,4,8"));
+    it("in", () => expect(formatFilter({ comment_in: [4, 8, 16, 32] })).toEqual("comment,in,4,8,16,32"));
+    it("is null", () => expect(formatFilter({ comment_is: 4 })).toEqual("comment,is"));
+    it("unsupported operator", () => expect(formatFilter({ comment_xy: 4 })).toEqual("comment_xy,eq,4"));
+    it("type errors", () => {
+        try {
+            formatFilter({ comment_bt: 4 });
+        }
+        catch (e) {
+            expect(e instanceof TypeError).toBeTruthy();
+            expect((e as TypeError).message).toEqual("Array expected as filter value for filter type \"between\" (bt)");
+        }
+
+        try {
+            formatFilter({ comment_in: 4 });
+        }
+        catch (e) {
+            expect(e instanceof TypeError).toBeTruthy();
+            expect((e as TypeError).message).toEqual("Array expected as filter value for filter type \"in\"");
+        }
+    });
+    it("ignore additional arguments", () => {
+        expect(formatFilter({ comment_bt: [4, 8, 16, 32] })).toEqual("comment,bt,4,8");
+        expect(formatFilter({ comment_is: 4 })).toEqual("comment,is");
+    });
+});
+
+describe("getURL", () => {
+    it("params should be encoded correctly", () => {
+        const params = { order: "length,DESC", page: "1,25", filter: formatFilter({ id: 4 }) };
+        expect(dataProvider.getURL("comment", params)).toEqual("http://myApi.com/records/comment?order=length,DESC&page=1,25&filter=id,eq,4");
+    });
+});
+
+describe("dataProvider API", () => {
+
+    let mock: jest.SpyInstance;
+    beforeEach(() => {
+        mock = spyOn(mockHTTP, "fetch");
+    });
+
+    it("create", async () => {
+        mock.mockImplementationOnce(() => ({ json: 1 }));
+        const result = await dataProvider.create("comment", { data: { id: 1, title: "myComment", content: "This is a test comment" } });
+
+        expect(mock).lastCalledWith(
+            "http://myApi.com/records/comment",
+            {
+                body: JSON.stringify({ id: 1, title: "myComment", "content": "This is a test comment" }),
+                method: "POST"
+            }
+        );
+
+        expect(result).toEqual({
+            data: {
+                title: "myComment",
+                content: "This is a test comment",
+                id: 1
+            },
+        });
+    });
+
+    it("delete", async () => {
+        const result = await dataProvider.delete("comment", { id: 1, previousData: { id: 1, title: "myComment", content: "This is a test comment" } });
+
+        expect(mock).lastCalledWith(
+            "http://myApi.com/records/comment/1",
+            {
+                method: "DELETE"
+            }
+        );
+
+        expect(result).toEqual({
+            data: {
+                title: "myComment",
+                content: "This is a test comment",
+                id: 1
+            },
+        });
+    });
+
+    it("delete", async () => {
+        const result = await dataProvider.deleteMany("comment", { ids: [1, 2] });
+
+        expect(mock).lastCalledWith(
+            "http://myApi.com/records/comment/1,2",
+            {
+                method: "DELETE"
+            }
+        );
+
+        expect(result).toEqual({
+            data: [1, 2],
+        });
+    });
+
+    it("getList", async () => {
+        mock.mockImplementationOnce(() => ({ json: { records: [{ id: 1, title: "myComment", content: "This is a test comment" }], results: 1 } }));
+
+        const result = await dataProvider.getList("comment", {
+            filter: {
+                id_in: [1, 2, 3, 4, 5],
+                title_cs: "Comm"
+            },
+            pagination: {
+                page: 1,
+                perPage: 10
+            },
+            sort: {
+                field: "title",
+                order: "ASC"
+            }
+        });
+
+        expect(mock).lastCalledWith(
+            "http://myApi.com/records/comment?order=title,ASC&page=1,10&filter=id,in,1,2,3,4,5&filter=title,cs,Comm",
+            undefined
+        );
+
+        expect(result).toEqual({
+            data:
+                [{
+                    id: 1,
+                    content: "This is a test comment",
+                    title: "myComment",
+                }],
+            total: 1,
+        });
+    });
+
+    it("getMany", async () => {
+        mock.mockImplementationOnce(() => ({
+            json: [
+                { id: 1, title: "myComment", content: "This is a test comment" },
+                { id: 2, title: "secondComment", content: "This is another test comment" }
+            ]
+        }));
+
+        const result = await dataProvider.getMany("comment", { ids: [1, 2] });
+
+        expect(mock).lastCalledWith(
+            "http://myApi.com/records/comment/1,2",
+            undefined
+        );
+
+        expect(result).toEqual({
+            data:
+                [{
+                    id: 1,
+                    content: "This is a test comment",
+                    title: "myComment",
+                }, {
+                    id: 2,
+                    content: "This is another test comment",
+                    title: "secondComment",
+                }]
+        });
+    });
+
+    it("getMany should still return array when only fetching one record", async () => {
+        mock.mockImplementationOnce(() => ({
+            json: { id: 1, title: "myComment", content: "This is a test comment" }
+        }));
+
+        const result = await dataProvider.getMany("comment", { ids: [1] });
+
+        expect(mock).lastCalledWith(
+            "http://myApi.com/records/comment/1",
+            undefined
+        );
+
+        expect(result).toEqual({
+            data:
+                [{
+                    id: 1,
+                    content: "This is a test comment",
+                    title: "myComment",
+                }]
+        });
+    });
+
+    it("getManyReference", async () => {
+        mock.mockImplementationOnce(() => ({
+            json: {
+                records: [
+                    { id: 1, title: "myComment", content: "This is a test comment" },
+                    { id: 2, title: "secondComment", content: "This is another test comment" }
+                ],
+                results: 2
+            }
+        }));
+
+        const result = await dataProvider.getManyReference("comment", {
+            id: "1",
+            target: "post",
+            filter: {
+                title_cs: "Comm"
+            },
+            pagination: {
+                page: 1,
+                perPage: 10
+            },
+            sort: {
+                field: "title",
+                order: "ASC"
+            }
+        });
+
+        expect(mock).lastCalledWith(
+            "http://myApi.com/records/comment?order=title,ASC&page=1,10&filter=title,cs,Comm&filter=post,eq,1",
+            undefined
+        );
+
+        expect(result).toEqual({
+            data:
+                [{
+                    id: 1,
+                    content: "This is a test comment",
+                    title: "myComment",
+                }, {
+                    id: 2,
+                    content: "This is another test comment",
+                    title: "secondComment",
+                }],
+            total: 2
+        });
+    });
+
+    it("getOne", async () => {
+        mock.mockImplementationOnce(() => ({
+            json: {
+                id: 1, title: "myComment", content: "This is a test comment"
+            }
+        }));
+
+        const result = await dataProvider.getOne("comment", {
+            id: "1"
+        });
+
+        expect(mock).lastCalledWith(
+            "http://myApi.com/records/comment/1",
+            undefined
+        );
+
+        expect(result).toEqual({
+            data: {
+                id: 1,
+                content: "This is a test comment",
+                title: "myComment",
+            },
+        });
+    });
+
+    it("update", async () => {
+        mock.mockImplementationOnce(() => ({
+            json: {
+                id: 1, title: "New Title", content: "This is a test comment"
+            }
+        }));
+
+        const result = await dataProvider.update("comment", {
+            id: "1",
+            data: {
+                title: "New Title"
+            },
+            previousData: {
+                id: 1, title: "myComment", content: "This is a test comment"
+            }
+        });
+
+        expect(mock).lastCalledWith(
+            "http://myApi.com/records/comment/1",
+            {
+                body: JSON.stringify({
+                    title: "New Title"
+                }),
+                method: "PUT"
+            }
+        );
+
+        expect(result).toEqual({
+            data: {
+                id: 1,
+                content: "This is a test comment",
+                title: "New Title",
+            },
+        });
+    });
+
+    it("update", async () => {
+        mock.mockImplementationOnce(() => ({
+            json: {
+                id: 1, title: "New Title", content: "This is a test comment"
+            }
+        }));
+
+        const result = await dataProvider.updateMany("comment", {
+            ids: [1, 2],
+            data: [{
+                id: 1,
+                title: "New Title"
+            }, {
+                id: 2,
+                title: "Another new Title"
+            }],
+        });
+
+        expect(mock).lastCalledWith(
+            "http://myApi.com/records/comment/1,2",
+            {
+                body: JSON.stringify([{
+                    id: 1,
+                    title: "New Title"
+                }, {
+                    id: 2,
+                    title: "Another new Title"
+                }]),
+                method: "PUT"
+            }
+        );
+
+        expect(result).toEqual({
+            data: [1, 2],
+        });
+    });
+
+});
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {
+import type {
     CreateParams,
     CreateResult,
     DataProvider,
@@ -6,7 +6,6 @@ import {
     DeleteManyResult,
     DeleteParams,
     DeleteResult,
-    fetchUtils,
     GetListParams,
     GetListResult,
     GetManyParams,
@@ -20,7 +19,11 @@ import {
     UpdateManyResult,
     UpdateParams,
     UpdateResult
-} from 'ra-core';
+} from "ra-core";
+
+import {
+    fetchUtils
+} from "ra-core";
 
 /**
  * Maps react-admin queries to a TreeQL powered REST API
@@ -59,14 +62,14 @@ import {
 /**
  * append the result directly to the request url
  */
-const formatFilterAND = (filter: Record<string, any>): string => {
+export const formatFilter = (filter: Record<string, any>): string => {
     return Object.entries(filter)
         .map(([key, value], _index) => {
             let operator: FilterOperator = "eq";
             const suffix = key.slice(-2);
             if (key.slice(-3, -2) === "_" && isValidFilterOperator(suffix)) {
                 operator = suffix;
-                return [key.slice(0, -3), operator, formatFilterArguments(operator, value)].join(",");
+                return [key.slice(0, -3), operator, formatFilterArguments(operator, value)].filter(v => !!v).join(",");
             }
 
             return `${key},eq,${value}`;
@@ -85,7 +88,7 @@ const formatFilterArguments = (operator: FilterOperator, value: any): string => 
             return value.join(",");
 
         case "is":
-            return ""; // is null filter doesn't require any arguments
+            return null; // is null filter doesn't require any arguments
 
         default:
             return value;
@@ -111,29 +114,33 @@ type FilterOperator = typeof filterOperators[number];
 
 const isValidFilterOperator = (value: any): value is FilterOperator => filterOperators.includes(value);
 
-const formatParams = (rawParams?: Record<string, string>): string => new URLSearchParams(rawParams).toString();
+const formatParams = (rawParams?: Record<string, string>): string => decodeURIComponent(new URLSearchParams(rawParams).toString());
 
-class TreeQLDataProvider<ResourceType extends string = string> implements DataProvider<ResourceType> {
+export class TreeQLDataProvider<ResourceType extends string = string> implements DataProvider<ResourceType> {
 
-    public constructor(private apiUrl: ResourceType, private httpClient = fetchUtils.fetchJson) { }
+    private apiUrl: string;
+    public constructor(_apiUrl: ResourceType, private httpClient = fetchUtils.fetchJson) {
+        this.apiUrl = _apiUrl.endsWith("/") ? _apiUrl.slice(0, -1) : _apiUrl;
+    }
 
     public async getList<RecordType extends RaRecord = any>(resource: ResourceType, params: GetListParams): Promise<GetListResult<RecordType>> {
-        const { page, perPage } = params.pagination;
-        const { field, order } = params.sort;
+        const { sort, pagination, filter } = params;
+        const { page, perPage } = pagination;
+        const { field, order } = sort;
         const url = this.getURL(resource, {
             order: `${field},${order}`,
             page: `${page},${perPage}`,
-            filter: formatFilterAND(params.filter)
+            filter: formatFilter(filter)
         });
 
         const { json: { records, results } } = await this.httpClient(url);
         return ({
             data: records,
-            total: results ?? 0,
+            total: results,
         });
     }
 
-    public async getOne<RecordType extends RaRecord = any>(resource: ResourceType, params: GetOneParams): Promise<GetOneResult<RecordType>> {
+    public async getOne<RecordType extends RaRecord = any>(resource: ResourceType, params: GetOneParams<RecordType>): Promise<GetOneResult<RecordType>> {
         const { json } = await this.httpClient(`${this.getURL(resource)}/${params.id}`);
         return ({ data: json });
     }
@@ -145,64 +152,79 @@ class TreeQLDataProvider<ResourceType extends string = string> implements DataPr
     }
 
     public async getManyReference<RecordType extends RaRecord = any>(resource: ResourceType, params: GetManyReferenceParams): Promise<GetManyReferenceResult<RecordType>> {
-        const { page, perPage } = params.pagination;
-        const { field, order } = params.sort;
+        const { id, target, sort, pagination, filter } = params;
+        const { page, perPage } = pagination;
+        const { field, order } = sort;
         const url = this.getURL(resource, {
             order: `${field},${order}`,
             page: `${page},${perPage}`,
-            filter: formatFilterAND(params.filter)
+            filter: formatFilter({
+                ...filter,
+                [target]: id
+            })
         });
 
-        const { json } = await this.httpClient(url);
+        const { json: { records, results } } = await this.httpClient(url);
         return ({
-            data: json.records,
-            total: json.results,
+            data: records,
+            total: results,
         });
     }
 
-    public async update<RecordType extends RaRecord = any>(resource: ResourceType, params: UpdateParams): Promise<UpdateResult<RecordType>> {
-        await this.httpClient(`${this.getURL(resource)}/${params.id}`, {
+    public async update<RecordType extends RaRecord = any>(resource: ResourceType, params: UpdateParams<RecordType>): Promise<UpdateResult<RecordType>> {
+        const { id, data, previousData } = params
+        await this.httpClient(`${this.getURL(resource)}/${id}`, {
             method: 'PUT',
-            body: JSON.stringify(params.data),
+            body: JSON.stringify(data),
         });
-        return this.getOne(resource, params);
+        return {
+            data: {
+                ...previousData,
+                ...data
+            }
+        };
     }
 
-    public async updateMany<RecordType extends RaRecord = any>(resource: ResourceType, params: UpdateManyParams): Promise<UpdateManyResult<RecordType>> {
-        await this.httpClient(`${this.getURL(resource)}/${params.ids.join(',')}`, {
+    public async updateMany<RecordType extends RaRecord = any>(resource: ResourceType, params: UpdateManyParams<RecordType[]>): Promise<UpdateManyResult<RecordType>> {
+        const { ids, data } = params;
+        await this.httpClient(`${this.getURL(resource)}/${ids.join(',')}`, {
             method: 'PUT',
-            body: JSON.stringify(params.data),
+            body: JSON.stringify(data),
         });
-        return { data: params.ids };
+        // TODO: there's no previous data for updateMany... do we need to return complete records?
+        return { data: ids };
     }
 
-    public async create<RecordType extends RaRecord = any>(resource: ResourceType, params: CreateParams): Promise<CreateResult<RecordType>> {
+    public async create<RecordType extends RaRecord = any>(resource: ResourceType, params: CreateParams<RecordType>): Promise<CreateResult<RecordType>> {
+        const { data } = params;
         const { json } = await this.httpClient(`${this.getURL(resource)}`, {
             method: 'POST',
-            body: JSON.stringify(params.data),
+            body: JSON.stringify(data),
         });
         return ({
-            data: { ...params.data, id: json },
+            data: { ...data, id: json },
         });
     }
 
-    public async delete<RecordType extends RaRecord = any>(resource: ResourceType, params: DeleteParams): Promise<DeleteResult<RecordType>> {
-        const { id } = params
+    public async delete<RecordType extends RaRecord = any>(resource: ResourceType, params: DeleteParams<RecordType>): Promise<DeleteResult<RecordType>> {
+        const { id, previousData } = params
         await this.httpClient(`${this.getURL(resource)}/${id}`, {
             method: 'DELETE',
         });
-        return ({ data: { id } as RecordType });
+        return ({ data: previousData });
     }
 
-    public async deleteMany<RecordType extends RaRecord = any>(resource: ResourceType, params: DeleteManyParams): Promise<DeleteManyResult<RecordType>> {
-        const { json } = await this.httpClient(`${this.getURL(resource)}/${params.ids.join(',')}`, {
+    public async deleteMany<RecordType extends RaRecord = any>(resource: ResourceType, params: DeleteManyParams<RecordType>): Promise<DeleteManyResult<RecordType>> {
+        const { ids } = params;
+        await this.httpClient(`${this.getURL(resource)}/${ids.join(',')}`, {
             method: 'DELETE',
         });
-        return ({ data: Array.isArray(json) ? json : [json] });
+        return ({ data: ids });
     }
 
-    private getURL(resource: string, params?: Record<string, string>) {
-        return `${this.apiUrl}/records/${resource}?${formatParams(params)}`;
+    protected getURL(resource: string, _params?: Record<string, string>) {
+        const params = (_params && Object.keys(_params).length > 0) ? `?${formatParams(_params)}` : "";
+        return `${this.apiUrl}/records/${resource}${params}`;
     }
 }
 
